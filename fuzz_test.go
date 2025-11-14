c653a59742afed72aa739bd52396f5e64dfabd9b
Frédéric BIDON <fredbi@yahoo.com>
2025-11-14 22:19:19 +0100

    test: added fuzz test for parsing
    Signed-off-by: Frédéric BIDON <fredbi@yahoo.com>

---
+++ fuzz_test.go
@@ -0,0 +1,34 @@
+package jsonpointer
+
+import (
+	"iter"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/go-openapi/testify/v2/require"
+)
+
+func FuzzParse(f *testing.F) {
+	cumulated := make([]string, 0, 100)
+	for generator := range generators() {
+		f.Add(generator)
+
+		cumulated = append(cumulated, generator)
+		f.Add(strings.Join(cumulated, ""))
+	}
+
+	f.Fuzz(func(t *testing.T, input string) {
+		require.NotPanics(t, func() {
+			_, _ = New(input)
+		})
+	})
+}
+
+func generators() iter.Seq[string] {
+	return slices.Values([]string{
+		`a`,
+		``, `/`, `/`, `/a~1b`, `/a~1b`, `/c%d`, `/e^f`, `/g|h`, `/i\j`, `/k"l`, `/ `, `/m~0n`,
+		`/foo`, `/0`,
+	})
+}
