635e674c10c046e258453677b5f30aecf4fd2f15
Oleksandr Redko <oleksandr.red+github@gmail.com>
2026-04-07 12:37:07 +0300

    fix(errors): correct error message for invalid JSON pointer start (#118)
    Signed-off-by: Oleksandr Redko <oleksandr.red+github@gmail.com>

--- errors.go
+++ errors.go
@@ -16,7 +16,7 @@ const (
 	ErrPointer pointerError = "JSON pointer error"
 
 	// ErrInvalidStart states that a JSON pointer must start with a separator ("/").
-	ErrInvalidStart pointerError = `JSON pointer must be empty or start with a "` + pointerSeparator
+	ErrInvalidStart pointerError = `JSON pointer must be empty or start with a "` + pointerSeparator + `"`
 
 	// ErrUnsupportedValueType indicates that a value of the wrong type is being set.
 	ErrUnsupportedValueType pointerError = "only structs, pointers, maps and slices are supported for setting values"
